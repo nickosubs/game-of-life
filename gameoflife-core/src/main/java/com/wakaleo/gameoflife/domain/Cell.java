47a2ec29ac380d5394bac2f2f0a5d5ce2b72fc33
thebuilder <thebuilder@build-virtualbox.org>
2012-07-05 13:19:44 +0100

    Added a comment

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -3,6 +3,7 @@ package com.wakaleo.gameoflife.domain;
 public enum Cell {
     LIVE_CELL("*"), DEAD_CELL(".");
 
+    /* Just adding a comment */
     private String symbol;
 
     private Cell(final String initialSymbol) {
